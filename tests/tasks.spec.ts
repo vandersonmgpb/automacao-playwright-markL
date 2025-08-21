e89539bf0419c94703899a866056f17e24b1279c
vandersonmgpb <vanderson1022@gmail.com>
2025-08-21 20:35:58 -0300

    Teste ponta-a-ponta

---
+++ tests/tasks.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test'
+
+test('deve poder cadastrar uma nova tarefa', async ({ page, request })=>{
+
+    // Dado que eu tenho uma nova tarefa
+    const taskName = 'Ler um livro de TypeScript'
+    await request.delete('http://localhost:3333/helper/tasks/' + taskName)
+ 
+    // E que estou na página de cadastro
+    await page.goto('http://localhost:8080')
+ 
+    // Quando faço o cadastro dessa tarefa
+    const inputTaskName = page.locator('input[class*=InputNewTask]')
+    await inputTaskName.fill('Ler um livro de TypeScript')
+    // await page.fill('input[class*=InputNewTask]', 'Ler um livro de Typescript')  
+    // await page.click('xpath=//button[contains(text(), "Create")]')
+    await page.click('css=button >> text=Create')
+ 
+    // Então essa tarefa deve ser exibida na lista
+    
+
+
+
+
+})
