31484f28f6345fb17463b46d3b2fa1ba4cbcba6e
vandersonmgpb <vanderson1022@gmail.com>
2025-08-27 20:28:39 -0300

    criando Custom Commands (Helpers)

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,33 +1,39 @@
 import { test, expect } from '@playwright/test'
+import { TaskModel } from './fixtures/task.model'
+import { deleteTaskByHelper, postTask } from './support/helpers'
 
 
 test('deve poder cadastrar uma nova tarefa', async ({ page, request }) => {
 
-    const taskName = 'Ler um livro de TypeScript'
-    await request.delete('http://localhost:3333/helper/tasks/' + taskName)
+    const task: TaskModel = {
+        name: 'Ler um livro de TypeScript',
+        is_done: false
+    }
+
+    await deleteTaskByHelper(request, task.name)
 
     await page.goto('http://localhost:8080')
 
     const inputTaskName = page.locator('input[class*=InputNewTask]')
-    await inputTaskName.fill('Ler um livro de TypeScript')
+    await inputTaskName.fill(task.name)
     // await page.fill('input[class*=InputNewTask]', 'Ler um livro de Typescript')  
     // await page.click('xpath=//button[contains(text(), "Create")]')
     await page.click('css=button >> text=Create')
 
-    const target = page.locator(`css=.task-item p >> text=${taskName}`)
+    const target = page.locator(`css=.task-item p >> text=${task.name}`)
     await expect(target).toBeVisible()
 })
 
-test.only('não deve permitir tarefa duplicada', async ({ page, request }) => {
+test('não deve permitir tarefa duplicada', async ({ page, request }) => {
 
-    const task = {
+    const task: TaskModel = {
         name: 'Comprar Ketchup',
         is_done: false
     }
-    await request.delete('http://localhost:3333/helper/tasks/' + task.name)
 
-    const newTask = await request.post('http://localhost:3333/tasks', { data: task })
-    expect(newTask.ok()).toBeTruthy()
+    await deleteTaskByHelper(request, task.name)
+    await postTask(request, task)
+    
 
     await page.goto('http://localhost:8080')
 
