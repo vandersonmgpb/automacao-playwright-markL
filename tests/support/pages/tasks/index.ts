05334c0e6392810543469d365d1bd6439e19d410
vandersonmgpb <vanderson1022@gmail.com>
2025-08-28 11:34:17 -0300

    Xtrabalhando com xath Ãe css para testes e2e

--- tests/support/pages/tasks/index.ts
+++ tests/support/pages/tasks/index.ts
@@ -1,11 +1,13 @@
-import { Page, expect } from '@playwright/test'
+import { Page, Locator, expect } from '@playwright/test'
 import { TaskModel } from '../../../fixtures/task.model'
 
 export class TasksPage {
     readonly page: Page
+    readonly inputTaskName: Locator
 
     constructor(page: Page) {
         this.page = page
+        this.inputTaskName = page.locator('input[class*=InputNewTask]')
     }
 
     async go() {
@@ -13,11 +15,15 @@ export class TasksPage {
     }
 
     async create(task: TaskModel) {
-        const inputTaskName = this.page.locator('input[class*=InputNewTask]')
-        await inputTaskName.fill(task.name)
+        await this.inputTaskName.fill(task.name)
         // await page.fill('input[class*=InputNewTask]', 'Ler um livro de Typescript') // Alternativa
         // await page.click('xpath=//button[contains(text(), "Create")]') // Alternativa
         await this.page.click('css=button >> text=Create')
+    }
+
+    async toggle(taskName: string) {
+        const target = this.page.locator(`xpath=//p[text()="${taskName}"]/..//button[contains(@class, "Toggle")]`)
+        await target.click()
     }
 
     async shouldHaveText(taskName: string) {
@@ -28,5 +34,10 @@ export class TasksPage {
     async alertHaveText(text: string) {
         const target = this.page.locator('.swal2-html-container')
         await expect(target).toHaveText(text)
+    }
+
+    async shouldBeDone(taskName: string) {
+        const target = this.page.getByText(taskName)
+        await expect(target).toHaveCSS('text-decoration-line', 'line-through')
     }
 }
--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,36 +1,60 @@
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 import { TaskModel } from './fixtures/task.model'
 import { deleteTaskByHelper, postTask } from './support/helpers'
 import { TasksPage } from './support/pages/tasks'
 
-test('deve poder cadastrar uma nova tarefa', async ({ page, request }) => {
-    const task: TaskModel = {
-        name: 'Ler um livro de TypeScript',
-        is_done: false
-    }
+import data from './fixtures/tasks.json'
 
-    await deleteTaskByHelper(request, task.name)
-    const tasksPage: TasksPage = new TasksPage(page)
-
-    await tasksPage.go()
-    await tasksPage.create(task)
-    await tasksPage.shouldHaveText(task.name)
-
+test.describe('cadastro', () => {
+    test('deve poder cadastrar uma nova tarefa', async ({ page, request }) => {
+        const task: TaskModel = data.success as TaskModel
+    
+        await deleteTaskByHelper(request, task.name)
+        const tasksPage: TasksPage = new TasksPage(page)
+    
+        await tasksPage.go()
+        await tasksPage.create(task)
+        await tasksPage.shouldHaveText(task.name)
+    
+    })
+    
+    test('não deve permitir tarefa duplicada', async ({ page, request }) => {
+        const task: TaskModel = data.duplicate as TaskModel
+    
+        await deleteTaskByHelper(request, task.name)
+        await postTask(request, task)
+        
+        const tasksPage: TasksPage = new TasksPage(page)
+    
+        await tasksPage.go()
+        await tasksPage.create(task)
+        await tasksPage.alertHaveText('Task already exists!')
+    })
+    
+    test('campo obrigatório', async ({ page }) => {
+        const task: TaskModel = data.required as TaskModel
+    
+        const tasksPage: TasksPage = new TasksPage(page)
+        await tasksPage.go()
+        await tasksPage.create(task)
+    
+        const validationMessage = await tasksPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage) 
+        expect(validationMessage).toEqual('This is a required field')
+    })
 })
 
-test('não deve permitir tarefa duplicada', async ({ page, request }) => {
-    const task: TaskModel = {
-        name: 'Comprar Ketchup',
-        is_done: false
-    }
-
-    await deleteTaskByHelper(request, task.name)
-    await postTask(request, task)
+test.describe('atualização', () => {
+    test('deve concluir uma tarefa', async ({ page, request }) => {
+        const task = data.update as TaskModel
     
-    const tasksPage: TasksPage = new TasksPage(page)
-
-    await tasksPage.go()
-    await tasksPage.create(task)
-    await tasksPage.alertHaveText('Task already exists!')
+        await deleteTaskByHelper(request, task.name)
+        await postTask(request, task)
+    
+        const tasksPage: TasksPage = new TasksPage(page)
+        
+        await tasksPage.go()
+        await tasksPage.toggle(task.name)
+        await tasksPage.shouldBeDone(task.name)
+    })
 })
